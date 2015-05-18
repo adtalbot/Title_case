41e57f3c4e77aa1ac52a4206a67c6800b0d6ffed
Audrie Talbot <audrie.talbot@gmail.com>
2015-05-18 10:54:54 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/title_case')
-also_reload('lib/**/*.rb') #Ask about this later
+also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
